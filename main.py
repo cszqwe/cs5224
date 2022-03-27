2dc3f52250d1bfae1acdeceb7cdf62d3414f0c30
cszqwe <cszznbb@gmail.com>
2022-03-27 15:32:12 +0800

    [server] Implement a basic TagToChat api.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from pickle import TRUE
 import time
 import socket
+import json
 
 hostName = socket.gethostname()
 serverPort = 8080
@@ -16,6 +18,26 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
+
+    def do_POST(self):
+        requestPath = self.path
+        print(self.path)
+        if requestPath == "/tagtochat":
+            self.handleTagToChat()
+        self.send_response(404)
+        
+    def handleTagToChat(self):
+        content_len = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_len)
+        print(post_body)
+        parsedRequst = json.loads(post_body)
+        userId = parsedRequst['userId']
+        tag = parsedRequst['tag']
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        response = json.dumps({'matchedUserId' : '123', 'matchedStatus' : 'FINISHED'})
+        self.wfile.write(bytes(response, encoding='utf8'))
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
