f637572da36fed82bc8df743137e582424dc6b29
cszqwe <cszznbb@gmail.com>
2022-03-29 23:53:54 +0800

    [redis] Test rpush lpop behaviour

--- main.py
+++ main.py
@@ -11,30 +11,20 @@ serverPort = 8080
 pytrends = TrendReq(hl='en-US', tz=360)
 r = redis.Redis(host='taptotagredis.riupow.ng.0001.apse1.cache.amazonaws.com',
     port=6379)
-r.mset({"Croatia": "Zagreb", "Bahamas": "Nassau"})
-print(r.get("Bahamas"))
-# kw_list = ["Blockchain"]
-# print(pytrends.suggestions("Helllo"))
-# print(pytrends.realtime_trending_searches(pn='US'))
+r.rpush('test1', '1')
+print(r.lpop())
+print(r.lpop())
 class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
-
     def do_POST(self):
         requestPath = self.path
         print(self.path)
-        if requestPath == "/tagtochat":
-            self.handleTagToChat()
+        if requestPath == "/match":
+            self.handleMatch()
+        if requestPath == "/gettag":
+            self.handleMatch()
         self.send_response(404)
         
-    def handleTagToChat(self):
+    def handleMatch(self):
         content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len)
         print(post_body)
@@ -45,6 +35,18 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_header("Content-type", "application/json")
         self.end_headers()
         response = json.dumps({'sessionId' : '123'})
+        self.wfile.write(bytes(response, encoding='utf8'))
+
+    def handleGetTag(self):
+        content_len = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_len)
+        print(post_body)
+        parsedRequst = json.loads(post_body)
+        input = parsedRequst['input']
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        response = json.dumps({'tag' : input})
         self.wfile.write(bytes(response, encoding='utf8'))
 
 if __name__ == "__main__":
