4d789b14ecf24c7097ab5c686d4df4e722689ccb
cszqwe <cszznbb@gmail.com>
2022-03-30 00:03:10 +0800

    [server] Implement the real match logic, by taking use of a rpush+lpop operations of redis.

--- main.py
+++ main.py
@@ -5,26 +5,30 @@ from pytrends.request import TrendReq
 import socket
 import json
 import redis
+import uuid
 
 hostName = socket.gethostname()
 serverPort = 8080
 pytrends = TrendReq(hl='en-US', tz=360)
 r = redis.Redis(host='taptotagredis.riupow.ng.0001.apse1.cache.amazonaws.com',
     port=6379)
-r.rpush('test1', '1')
-print(r.lpop('test1'))
-print(r.lpop('test1'))
+# r.rpush('test1', '1')
+# print(r.lpop('test1'))
+# print(r.lpop('test1'))
 class MyServer(BaseHTTPRequestHandler):
     def do_POST(self):
         requestPath = self.path
         print(self.path)
+        if requestPath == "/register":
+            self.handleRegister()
         if requestPath == "/match":
             self.handleMatch()
         if requestPath == "/gettag":
-            self.handleMatch()
+            self.handleGetTag()
         self.send_response(404)
-        
-    def handleMatch(self):
+
+    def handleRegister(self):
+        # TODO: Write the userId into the database
         content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len)
         print(post_body)
@@ -34,10 +38,30 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
-        response = json.dumps({'sessionId' : '123'})
+        response = json.dumps({'status' : 'successful'})
+        self.wfile.write(bytes(response, encoding='utf8'))
+
+    def handleMatch(self):
+        content_len = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_len)
+        print(post_body)
+        parsedRequst = json.loads(post_body)
+        userId = parsedRequst['userId']
+        tag = parsedRequst['tag']
+        # TODO: Update user searching history
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        # Search whether there is a matched session for this tag.
+        sessionId = r.lpop(tag)
+        if sessionId == None:
+            sessionId = uuid.uuid1()
+            r.rpush(tag, sessionId)
+        response = json.dumps({'sessionId' : sessionId})
         self.wfile.write(bytes(response, encoding='utf8'))
 
     def handleGetTag(self):
+        # TODO: Take use of pytrends, to make the random input string matches to a tag
         content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len)
         print(post_body)
